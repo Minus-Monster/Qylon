1facdf2070f0d72f04159685ab7e4ab8ef8b7389
minus-monster <iam@minus.monster>
2024-02-28 13:34:00 +0900

    Updated this Cmake file for static build

--- Qylon.cpp
+++ Qylon.cpp
@@ -7,7 +7,6 @@ void Qylon::Qylon::log(QString strings){
 }
 
 Qylon::Qylon::Qylon(){
-
     qWarning() << "[NOTICE] "
                 "Current bug lists : "
                 "1. Why does a trigger mode switch to 'Off'?"
